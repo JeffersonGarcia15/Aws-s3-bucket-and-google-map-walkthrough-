f696311dab38277f3aef813d6fb38c738e319b31
JeffersonGarcia15 <JeffersonGarcia1599@gmail.com>
2023-04-20 04:18:37 -0500

    adding-tests-structure/ Adding wait-for-postgres.sh

--- wait-for-postgres.sh
+++ wait-for-postgres.sh
@@ -3,10 +3,12 @@
 set -e
 
 host="$1"
+port="$2"
+shift
 shift
 cmd="$@"
 
-until PGPASSWORD=$POSTGRES_PASSWORD psql -h "$host" -U "$POSTGRES_USER" -d "$POSTGRES_DB" -c '\q'; do
+until PGPASSWORD=$POSTGRES_PASSWORD psql -h "$host" -p "$port" -U "$POSTGRES_USER" -d "$POSTGRES_DB" -c '\q'; do
   >&2 echo "Postgres is unavailable - sleeping"
   sleep 1
 done
